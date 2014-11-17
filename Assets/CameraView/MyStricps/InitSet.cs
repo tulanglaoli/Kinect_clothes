2aed77ee61e1178ba102f2947f1ecc5d63c845fd
tulanglaoli <tulanglaoli@foxmail.com>
2014-11-17 21:49:21 +0800

    Test
    aaa

---
+++ Assets/CameraView/MyStricps/InitSet.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class InitSet  {
+
+	public static void  InitTexture( Texture2D Tex)
+	{
+		Tex.wrapMode = TextureWrapMode.Repeat;
+		Tex.filterMode = FilterMode.Bilinear;
+		Tex.anisoLevel = 9;
+		//Tex.Compress (true);
+	}
+}
---
+++ Assets/Demo_1/Scripts/MyKinectInterface.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class MyKinectInterface  {
+
+	public Texture2D GetColorImage()
+	{
+		KinectManager manager = KinectManager.Instance;
+		return manager.GetUsersLblTex ();
+	}
+}
